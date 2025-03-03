ab8af143206df893f2a8dd95655eb4249aa9229c
ritesh0403 <riteshpatil0403@gmail.com>
2025-03-03 13:38:15 +0530

    better formatting

--- InterviewQuestions/HashsetMethods.java
+++ InterviewQuestions/HashsetMethods.java
@@ -1,43 +1,36 @@
 import java.util.*;
 import java.util.stream.*;
-// how to put vowels in both capital and small letters in hashset in java
-// isn't there any better way to do it rather writing 10 .add statement
 
 public class HashsetMethods {
     public static void main(String[] args) {
 
-// 1. Using Arrays.asList() (Compact Initialization)
-
-        // Initialize HashSet with vowels using Arrays.asList
+        // 1. Using Arrays.asList() - O(n) & O(n)  
         Set<Character> vowels1 = new HashSet<>(Arrays.asList('A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u'));
-
-        // Print the HashSet
         System.out.println("Vowel HashSet: " + vowels1);
+        // Output: [A, E, I, O, U, a, e, i, o, u]
+        // __
 
-
-// 2. Using Collections.addAll() (More Readable)
-
+        // 2. Using Collections.addAll() - O(n) & O(n)  
         Set<Character> vowels2 = new HashSet<>();
         Collections.addAll(vowels2, 'A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u');
-
         System.out.println("Vowel HashSet: " + vowels2);
-    
+        // Output: [A, E, I, O, U, a, e, i, o, u]
+        // __
 
-// 3. Using a Stream (Java 8+)
-
-
+        // 3. Using a Stream (Java 8+) - O(n) & O(n)  
         Set<Character> vowels3 = Stream.of('A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u')
-                                      .collect(Collectors.toSet());
-
+                                       .collect(Collectors.toSet());
         System.out.println("Vowel HashSet: " + vowels3);
+        // Output: [A, E, I, O, U, a, e, i, o, u]
+        // __
 
-// 4. Using a String to Avoid Typing Each Character
-
+        // 4. Using a String to Avoid Typing Each Character - O(n) & O(n)  
         Set<Character> vowels4 = new HashSet<>();
         for (char c : "AEIOUaeiou".toCharArray()) {
             vowels4.add(c);
         }
         System.out.println("Vowel HashSet: " + vowels4);
-
+        // Output: [A, E, I, O, U, a, e, i, o, u]
+        // __
     }
 }
